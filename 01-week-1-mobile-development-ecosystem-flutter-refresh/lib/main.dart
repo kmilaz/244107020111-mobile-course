dc9f2fd464eff851531d9b7ec0be856abfb61946
Kamila Zahwa <kamilazhwa@gmail.com>
2026-09-08 02:42:24 +0700

    feat: add additional text for mini assignment

--- 01-week-1-mobile-development-ecosystem-flutter-refresh/lib/main.dart
+++ 01-week-1-mobile-development-ecosystem-flutter-refresh/lib/main.dart
@@ -15,6 +15,8 @@ class MyApp extends StatelessWidget {
             Icon(Icons.school, size: 72),
             SizedBox(height: 16),
             Text('Kamila Zahwa', style: TextStyle(fontSize: 24)),
+            Text('244107020111'),
+            Text('D-IV Teknik Informatika'),
             Text('Pemrograman Mobile — Minggu 1'),
           ]),
         ),
