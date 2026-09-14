823e02a9aee073c58b7c72e228cd3a5200240185
Kamila Zahwa <kamilazhwa@gmail.com>
2026-09-14 13:10:17 +0700

    feat: praktikum layout (warm-up)

--- 02-week-2-declarative-ui-responsive-design/lib/main.dart
+++ 02-week-2-declarative-ui-responsive-design/lib/main.dart
@@ -1,16 +1,67 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MainApp());
-}
+void main() => runApp(const ProfileApp());
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class ProfileApp extends StatelessWidget {
+  const ProfileApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(body: Center(child: Text('Hello World!'))),
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Center(child: ProfileCard()),
+      ),
+    );
+  }
+}
+
+class ProfileCard extends StatelessWidget {
+  const ProfileCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 320,
+      padding: const EdgeInsets.all(16),
+      decoration: BoxDecoration(
+        color: Colors.indigo.shade50,
+        borderRadius: BorderRadius.circular(16),
+      ),
+      child: Column(
+        mainAxisSize:MainAxisSize.min,
+        children: [
+          Row(
+            children: [
+              const CircleAvatar(child: Icon(Icons.person)),
+              const SizedBox(width: 12),
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: const [
+                    Text('Nama Mahasiswa',
+                        style: TextStyle(fontWeight: FontWeight.bold)),
+                    Text('...ketik nama Anda di sini...'),
+                  ],
+                ),
+              ),
+            ],
+          ),
+          const SizedBox(height: 12),
+          const Row(children: [
+            Expanded(child: Text('NIM')),
+            Text('...ketik NIM Anda di sini...'),
+          ]),
+          const Row(children: [
+            Expanded(child: Text('Kelas')),
+            Text('...ketik kelas Anda di sini...'),
+          ]),
+          const Row(children: [
+            Expanded(child: Text('Email')),
+            Text('...ketik email Anda di sini...'),
+          ]),
+        ],
+      ),
     );
   }
 }
